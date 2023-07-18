acdb35d9a7b62b947df6c685b099f17a5c8fac2e
jheon-eom <https://github.com/jheon-eom>
2023-07-18 11:54:01 +0900

    초기 프로젝트 세팅

---
+++ src/test/java/com/example/PointsService/PointsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PointsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
